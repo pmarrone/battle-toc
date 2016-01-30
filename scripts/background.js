023630ece0055e2c8a62648267ac621ccbd4a73a
matiasiacono <matias@kinetica-solutions.com>
2016-01-30 02:29:40 -0300

    SOmething else

--- scripts/background.js
+++ scripts/background.js
@@ -6,5 +6,9 @@ var background = {
     draw: function (context) {
         context.fillStyle = '#F4E4C2';
         context.fillRect(0, 0, jsGFwk.settings.width, jsGFwk.settings.height);
+        
+        context.fillStyle = 'black';
+        context.fillRect(jsGFwk.settings.width / 2, 0,
+                         2, jsGFwk.settings.height);
     }
 };
