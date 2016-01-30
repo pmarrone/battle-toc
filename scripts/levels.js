8084d0ed88f6ca61fc83e18d86bbc922af971547
Patricio Marrone <marrone.patricio@gmail.com>
2016-01-30 13:48:28 -0300

    Cosas

--- scripts/levels.js
+++ scripts/levels.js
@@ -22,7 +22,15 @@ var levels;
         var blocks = [];
         var blockWidth = ((width - 2 * margins) / number) - gutter;
         var blockHeight = ((height - 2 * margins) / number) - gutter;
-        var trueBlock = parseInt(Math.random() * number * number); 
+        var trueBlock = parseInt(Math.random() * number * number);
+        
+        function getOtherColor() {
+            if (typeof otherColor === 'string') {
+                return otherColor;
+            }
+            return otherColor[parseInt(Math.random() * otherColor.length)];
+        }
+        
         for (var i = 0; i < number; i ++) {
             for (var j = 0; j < number; j++) {
                 var isTrueBlock = (i * number + j === trueBlock);
@@ -36,12 +44,12 @@ var levels;
                     height: blockHeight,
                     clickAction: isTrueBlock ? clickActions.updateGame : clickActions.penalizePlayer,
                     //FIXME
-                    draw: function (isTrueBlock) {
+                    draw: function (isTrueBlock, otherColor) {
                         return function (context) {
-                            context.fillStyle = isTrueBlock ? trueBlockColor : otherColor;
+                            context.fillStyle = isTrueBlock ? /*'rgba(255,0,0,'+ this.alpha +')'*/ trueBlockColor : otherColor;
                             context.fillRect(this.x, this.y, this.width, this.height);
                         };
-                    }(isTrueBlock)
+                    }(isTrueBlock, getOtherColor())
                 };
                 blocks.push(block);
             }
@@ -51,133 +59,48 @@ var levels;
     
     function addGridLevel(params) {
         var number = params.number || 1;
-        var trueBlockColor1 = params.trueBlockColor || 'blue';
-        var otherColor1 = params.otherColor || 'red';
-        var trueBlockColor2 = params.trueBlockColor || 'red';
-        var therColor2 = params.otherColor || 'blue';
+        var trueBlockColor1 = params.trueBlockColor1 || 'blue';
+        var otherColor1 = params.otherColor1 || 'red';
+        var trueBlockColor2 = params.trueBlockColor2 || 'red';
+        var otherColor2 = params.otherColor2 || 'blue';
 
         return {
             player1: function () {
             return createGridLevel(0, jsGFwk.settings.width / 2, jsGFwk.settings.height, number, 20, 'player1',  trueBlockColor1, otherColor1, 50);
             },
             player2: function () {
-                return createGridLevel(jsGFwk.settings.width / 2, jsGFwk.settings.width / 2, jsGFwk.settings.height, number, 20, 'player2', trueBlockColor1, otherColor1, 50);
-            }    
+                return createGridLevel(jsGFwk.settings.width / 2, jsGFwk.settings.width / 2, jsGFwk.settings.height, number, 20, 'player2', trueBlockColor2, otherColor2, 50);
+            },
+            player1target: function (context) {
+                context.fillStyle = trueBlockColor1;
+                context.fillRect(10, jsGFwk.settings.height - 50, 30, 30);
+            },
+            player2target: function (context) {
+                context.fillStyle = trueBlockColor2;
+                context.fillRect(jsGFwk.settings.width - 40, 50, 30, 30);
+            }
         };
     }
     
     
-    levels = [
-        {
-            player1target: function (context) {
-                context.fillStyle = 'red';
-                context.fillRect(10, jsGFwk.settings.height - 50, 30, 30);
-            },
-            player1: function () {
-                return createGridLevel(0, jsGFwk.settings.width / 2, jsGFwk.settings.height, 2, 20, 'player1', 'red', 'blue', 50);
-            },
-            player2target: function (context) {
-                context.fillStyle = 'blue';
-                context.fillRect(jsGFwk.settings.width - 40, 50, 30, 30);
-            },
-            player2: function () {
-                return createGridLevel(jsGFwk.settings.width / 2, jsGFwk.settings.width / 2, jsGFwk.settings.height, 2, 20, 'player2', 'blue', 'red', 50);
-            }
-        },
-        {
-            player1: function () {
-                return createGridLevel(0, jsGFwk.settings.width / 2, jsGFwk.settings.height, 3, 20, 'player1', 'red', 'blue', 50);
-            },
-            player2: function () {
-                return createGridLevel(jsGFwk.settings.width / 2, jsGFwk.settings.width / 2, jsGFwk.settings.height, 3, 20, 'player2', 'blue', 'red', 50);
-            }
-        },
-        {
-            player1: function () {
-                return createGridLevel(0, jsGFwk.settings.width / 2, jsGFwk.settings.height, 4, 20, 'player1', 'red', 'blue', 50);
-            },
-            player2: function () {
-                return createGridLevel(jsGFwk.settings.width / 2, jsGFwk.settings.width / 2, jsGFwk.settings.height, 4, 20, 'player2', 'blue', 'red', 50);
-            }
-        },
-        {
-            player1: function () {
-                return createGridLevel(0, jsGFwk.settings.width / 2, jsGFwk.settings.height, 5, 20, 'player1', 'red', 'blue', 50);
-            },
-            player2: function () {
-                return createGridLevel(jsGFwk.settings.width / 2, jsGFwk.settings.width / 2, jsGFwk.settings.height, 5, 20, 'player2', 'blue', 'red', 50);
-            }
-        },
-        { 
-            player1: function () {
-                return [{
-                    isClickable: true,
-                    player: 'player1', x: 100, y: 100, width: 200, height: 200,
-                    clickAction: function () {
-                        levelController.updateGame(this.belongToPlayer);
-                    },
-                    draw: function (context) {
-                        context.fillStyle = 'red';
-                        context.fillRect(this.x, this.y, this.width, this.height);
-                    }
-                }]; 
-            },
-
-            player2: function () {
-                return [{
-                    isClickable: true,
-                    player: 'player2', x: 800, y: 100, width: 200, height: 200,
-                    draw: function (context) {
-                        context.fillStyle = 'blue';
-                        context.fillRect(this.x, this.y, this.width, this.height);
-                    }
-                }];
-            }
-        },
-        { 
-            player1: function () {
-                return [{
-                    isClickable: true,
-                    player: 'player1', x: 100, y: 100, width: 100, height: 100,
-                    clickAction: function () {
-                        levelController.updateGame(this.belongToPlayer);
-                    },
-                    draw: function (context) {
-                        context.fillStyle = 'red';
-                        context.fillRect(this.x, this.y, this.width, this.height);
-                    }
-                }, {
-                    isClickable: true,
-                    x: 100,
-                    y: 500,
-                    player: 'player1',
-                    width: 100,
-                    height: 100,
-                    clickAction: function() {
-                        levelController.penalizePlayer(this.belongToPlayer);
-                    },
-                    draw: function (context) {
-                        context.fillStyle = 'green';
-                        context.fillRect(this.x, this.y, this.width, this.height);
-                    }
-                }]; 
-            },
-
-            player2: function () {
-                return [{
-                    isClickable: true,
-                    clickAction: function () {
-                        levelController.updateGame(this.belongToPlayer);
-                    },
-                    player: 'player2', x: 800, y: 100, width: 200, height: 200,
-                    draw: function (context) {
-                        context.fillStyle = 'blue';
-                        context.fillRect(this.x, this.y, this.width, this.height);
-                    }
-                }];
-            }
-        }
-
-    ];    
+    levels = [    ]; 
+    for (var i = 1; i <= 7; i++) {
+        levels.push(addGridLevel({number: i}));    
+    }
     
+    var otherReds = ['rgb(200, 0, 0 )', 'rgb(230, 40, 0 )', 'rgb(150, 30, 20 )', 'rgb(100, 20, 60 )'];
+    var otherBlues = ['rgb(0, 0, 210 )', 'rgb(0, 40, 200 )', 'rgb(50, 50, 200 )', 'rgb(60, 60, 150 )'];
+    var otherColor1 = ['red'];
+    var otherColor2 = ['blue'];
+    
+    for (i = 3; i <= 8; i++) {
+        levels.push(addGridLevel({number: i, trueBlockColor1: 'rgb(230, 40, 80 )', trueBlockColor2: 'rgb(80, 80, 210 )', otherColor1: otherColor1.slice(), otherColor2: otherColor2.slice()}));
+        if (otherReds.length > 0) {
+            otherColor1.push(otherReds.shift());  
+        }
+        if (otherBlues.length > 0) {
+            otherColor2.push(otherBlues.shift());
+        }
+    }
+       
 }());
